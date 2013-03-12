24cf73ea06eff2086d295448961b3f8c311cec8d
flinn <flinn@actsasflinn.com>
2013-03-11 23:39:19 -0400

    fix unixy stuff

--- run.sh
+++ run.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-COHERENCE_PATH="/usr/local/libcoherence"
-LIBRARY_PATH=$COHERENCE_PATH/lib/
+export COHERENCE_PATH="/usr/local/libcoherence"
+export LIBRARY_PATH=$COHERENCE_PATH/lib/
 
-OS=$(uname -s)
+export OS=$(uname -s)
 if [[ $OS == "Darwin" ]] ; then
-  DYLD_LIBRARY_PATH=$LIBRARY_PATH:$DYLD_LIBRARY_PATH
+  export DYLD_LIBRARY_PATH=$LIBRARY_PATH:$DYLD_LIBRARY_PATH
 elif [[ $OS == "Linux" ]] ; then
-  LD_LIBRARY_PATH=$LIBRARY_PATH:$LD_LIBRARY_PATH
+  export LD_LIBRARY_PATH=$LIBRARY_PATH:$LD_LIBRARY_PATH
 elif [[ $OS == "Sun" ]] ; then
-  LD_LIBRARY_PATH=$LIBRARY_PATH:$LD_LIBRARY_PATH
+  export LD_LIBRARY_PATH=$LIBRARY_PATH:$LD_LIBRARY_PATH
 else
   echo "unsupported platform"
 fi
 
-TangosolCoherenceCacheconfig=config/coherence-cache-config.xml
-TangosolCoherenceLogLevel=1
-TangosolCoherenceLog=stderr
+export TangosolCoherenceCacheconfig=config/coherence-cache-config.xml
+export TangosolCoherenceLogLevel=1
+export TangosolCoherenceLog=stderr
 
-./coherence-tool $*
+bin/coherence-tool $*
